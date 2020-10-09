d5a20621fc11b07d37c392dbd6b14416574d7d35
{ryosuke27} <opu.7455@gmail.com>
2020-10-09 21:50:16 +0900

    キーワード入力画面

--- scraping.php
+++ scraping.php
@@ -2,6 +2,11 @@
 
 require_once("./phpQuery-onefile.php");
 
+
+$key = $_POST["comment"];
+
+var_dump($key);
+
 $html = file_get_contents("https://www.itmedia.co.jp/");
 
 echo phpQuery::newDocument($html)->find("h3")->text();
